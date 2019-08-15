c227e72d3461ae87696476711bae71b4fa311cb3
unknown <fbayanati@yahoo.com>
2019-08-15 17:30:25 -0600

    add postProduct in controller class

--- 02/controllers/products.js
+++ 02/controllers/products.js
@@ -5,3 +5,17 @@ exports.getProducts = (req, res, next) => {
         ]
     });
 };
+
+exports.postProduct = (req, res, next) => {
+    const title = req.body.title;
+    const content = ewq.body.content;
+    
+    res.status(201).json( {
+        message: "successful",
+        post: {
+            id: new Date().toISOString(),
+            title: title,
+            content: content
+        }
+    });
+}
