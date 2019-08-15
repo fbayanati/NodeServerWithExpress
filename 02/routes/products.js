506c8fc87c089689d2b673205aad514497c99b86
unknown <fbayanati@yahoo.com>
2019-08-15 17:43:48 -0600

    add route for post a product

--- 02/routes/products.js
+++ 02/routes/products.js
@@ -3,5 +3,10 @@ const controller = require("../controllers/products");
 
 const router = express.Router();
 
+// GET /call/products
 router.get( "/products", controller.getProducts );
+
+// Post /call/product
+router.post( "/product", controller.postProduct );
+
 module.exports = router;
