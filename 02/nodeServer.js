6db1131479851cb4389128aa8330ae226ee3eb5f
unknown <fbayanati@yahoo.com>
2019-08-15 17:44:27 -0600

    add bosy-parser with json configuration (application/json)

--- 02/nodeServer.js
+++ 02/nodeServer.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const productRoutes = require("./routes/products");
 
 const app = express();
 
+app.use( bodyParser.json() );   // application/json
 app.use("/call", productRoutes);
 
 app.listen( 8080 );
