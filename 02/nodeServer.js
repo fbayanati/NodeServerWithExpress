0f1f426d0c073bb0b300ddfa2ef591997667f0d5
unknown <fbayanati@yahoo.com>
2019-08-15 19:15:25 -0600

    add CORS headers in response from node server

--- 02/nodeServer.js
+++ 02/nodeServer.js
@@ -6,6 +6,12 @@ const productRoutes = require("./routes/products");
 const app = express();
 
 app.use( bodyParser.json() );   // application/json
+app.use( (req, res, next) => {
+    res.setHeader( "Access-Control-Allow-Origin", "*" );
+    res.setHeader( "Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE" );
+    res.setHeader( "Access-Control-Allow-Headers", "Content-Type, Authorization, Accept, X-Requested-With, remember-me" );
+    next();
+});
 app.use("/call", productRoutes);
 
 app.listen( 8080 );
