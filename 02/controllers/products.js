4edb81e9b7efabe2cc48e17328da9d7d709c4301
unknown <fbayanati@yahoo.com>
2019-08-18 23:14:41 -0600

    add comments and clean up the code

--- 02/controllers/products.js
+++ 02/controllers/products.js
@@ -1,21 +1,22 @@
-exports.getProducts = (req, res, next) => {
-    res.status(200).json({
-        products: [
-            { id: 1001, name: "HP Folio 9470m"}
-        ]
-    });
+/*eslint no-undef: "error"*/
+/*eslint-env node*/
+
+exports.getProducts = (req, res) => {
+  res.status(200).json({
+    products: [{ id: 1001, name: "HP Folio 9470m" }]
+  });
 };
 
-exports.postProduct = (req, res, next) => {
-    const title = req.body.title;
-    const content = req.body.content;
-    
-    res.status(201).json( {
-        message: "successful",
-        post: {
-            id: new Date().toISOString(),
-            title: title,
-            content: content
-        }
-    });
-}
+exports.postProduct = (req, res) => {
+  const title = req.body.title;
+  const content = req.body.content;
+
+  res.status(201).json({
+    message: "successful",
+    post: {
+      id: new Date().toISOString(),
+      title: title,
+      content: content
+    }
+  });
+};
--- 02/routes/products.js
+++ 02/routes/products.js
@@ -1,12 +1,15 @@
+/*eslint no-undef: "error"*/
+/*eslint-env node*/
+
 const express = require("express");
 const controller = require("../controllers/products");
 
 const router = express.Router();
 
 // GET /call/products
-router.get( "/products", controller.getProducts );
+router.get("/products", controller.getProducts);
 
 // Post /call/product
-router.post( "/product", controller.postProduct );
+router.post("/product", controller.postProduct);
 
 module.exports = router;
