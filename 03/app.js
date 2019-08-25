1c0a70cd6518144f5f87c9ebcb0af617cc65bcc0
unknown <fbayanati@yahoo.com>
2019-08-24 18:52:18 -0600

    add CORS access allow in response header and bodyparser for application jason

--- 03/app.js
+++ 03/app.js
@@ -2,9 +2,19 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 let app = express();
 app.use( morgan('tiny') );
+app.use( bodyParser.json() );   // application/json
+
+// Add the CORS access allow to the response header 
+app.use( (req, res, next) => {
+    res.setHeader( "Access-Control-Allow-Origin", "*" );
+    res.setHeader( "Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE" );
+    res.setHeader( "Access-Control-Allow-Headers", "Content-Type, Authorization, Accept, X-Requested-With, remember-me" );
+    next();
+});
 
 app.get( '/', (req, res) => {
     res.status(200).send('Hello from Node/Express!');
