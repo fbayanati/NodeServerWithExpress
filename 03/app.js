31c61d0cf3fe7cd5913f3c4ac5e28ba75b16b3be
unknown <fbayanati@yahoo.com>
2019-08-24 18:45:13 -0600

    add debug/morgan packages

--- 03/app.js
+++ 03/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
 
 let app = express();
+app.use( morgan('tiny') );
 
 app.get( '/', (req, res) => {
     res.status(200).send('Hello from Node/Express!');
 });
 
 app.listen( 3000, () => {
-    console.log( 'Up and Running and Listening on port 3000 !');
+    console.log( `Up and Running and Listening on port ${chalk.green(3000)} !`);
 })
