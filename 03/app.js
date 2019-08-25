37f8fa1149dea98c0639a1c68c8db6e5636a2c44
unknown <fbayanati@yahoo.com>
2019-08-24 18:25:08 -0600

    add express listener

--- 03/app.js
+++ 03/app.js
@@ -1,3 +1,11 @@
-const express = erequire('express');
+const express = require('express');
 
 let app = express();
+
+app.get( '/', (req, res) => {
+    res.status(200).send('Hello from Node/Express!');
+});
+
+app.listen( 3000, () => {
+    console.log( 'Up and Running and Listening on port 3000 !');
+})
